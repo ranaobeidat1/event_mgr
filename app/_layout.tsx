23a8829960e326446cd8f2719dbf9208452e468f
yousef <yousefsalman030@gmail.com>
2025-06-20 02:53:31 +0300

    edit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -90,6 +90,7 @@ export default function RootLayout() {
   return (
     <AuthContext.Provider value={{ user }}>
       <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="forgot-password" options={{ headerShown: false }} />
          <Stack.Screen name="profile" options={{ headerShown: false }} />
         <Stack.Screen name="login" options={{ headerShown: false }} />
